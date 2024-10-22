752fb8a6af5eb79cffebeb1bf5d90595c85662e2
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-10-22 18:51:42 -0500

    feat: :sparkles: fix errors, now app its the brain of the project

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 // app.js
-const express = require('express');
-const path = require('path');
-require('dotenv').config(); // Cargar las variables de entorno
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
+
+// Obtener __dirname usando import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Servir archivos estáticos desde la carpeta 'src'
+app.use(express.static(join(__dirname, 'src'))); // Usa join para construir la ruta
+
+// Ruta para manejar cualquier otra solicitud
+app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, 'src', 'index.html')); // Usa join para construir la ruta
+});
 
 // Iniciar el servidor
 app.listen(PORT, () => {
