9c6080c1c6f8344dfaa0185777a17a1568174ca9
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-10-29 08:38:09 -0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import fs from 'fs';
 import path from 'path';
 import dotenv from 'dotenv';
 
@@ -8,20 +9,16 @@ dotenv.config();
 export default defineConfig({
   root: path.resolve(__dirname, 'src'),
   plugins: [vue()],
-  base: "https://k4rlit0s13.github.io/noteapp/", // URL base para GitHub Pages
+  base: "https://k4rlit0s13.github.io/noteapp/",
   server: {
-    // Comentar las configuraciones HTTPS
-    // https: {
-    //   key: fs.readFileSync(path.resolve(__dirname, 'private.key')),
-    //   cert: fs.readFileSync(path.resolve(__dirname, 'certificate.crt')),
-    // },
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'private.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certificate.crt')),
+    },
     port: process.env.FRONTEND_PORT || 3000,
     open: true, 
     watch: {
       usePolling: true,
     },
-  },
-  build: {
-    outDir: path.resolve(__dirname, 'dist'), // Asegúrate de que esta línea esté presente
   },
 });
