38249071144613068cc30c31daada8c92f85cd57
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-10-29 08:07:33 -0500

    changes more

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import fs from 'fs';
 import path from 'path';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 export default defineConfig({
   root: path.resolve(__dirname, 'src'),
   plugins: [vue()],
+  base: "https://k4rlit0s13.github.io/hospital/", // URL base para GitHub Pages
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'private.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certificate.crt')),
-    },
+    // Comentar las configuraciones HTTPS
+    // https: {
+    //   key: fs.readFileSync(path.resolve(__dirname, 'private.key')),
+    //   cert: fs.readFileSync(path.resolve(__dirname, 'certificate.crt')),
+    // },
     port: process.env.FRONTEND_PORT || 3000,
     open: true, 
     watch: {
