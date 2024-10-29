f794534de3b2227c49e96482eea6551ab8b8b142
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-10-29 08:44:38 -0500

    feat

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
   root: path.resolve(__dirname, 'src'),
   plugins: [vue()],
   base: "https://k4rlit0s13.github.io/noteapp/",
+  build: {
+    outDir: path.resolve(__dirname, 'dist'), // Esto debería ser correcto
+  },  
   server: {
     https: {
       key: fs.readFileSync(path.resolve(__dirname, 'private.key')),
