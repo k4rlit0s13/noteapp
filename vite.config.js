112f1d43657b8f655487814e1e7d1e2e286be001
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-10-29 09:22:10 -0500

    feat: :hammer: changes

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,15 @@ export default defineConfig({
   plugins: [vue()],
   base: "https://k4rlit0s13.github.io/noteapp/",
   build: {
-    outDir: path.resolve(__dirname, 'dist'), // Esto debería ser correcto
+    outDir: path.resolve(__dirname, 'dist'), // Esto debería generar la carpeta dist
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/index.html'), // Entrada principal
+        signup: path.resolve(__dirname, 'src/views/signup.html'), // Entrada para la página de registro
+        infonote: path.resolve(__dirname, 'src/views/infonote.html'), // Entrada para la página de información de notas
+        createnote: path.resolve(__dirname, 'src/views/createnote.html'), // Entrada para la página de creación de notas
+      }
+    }
   },
   server: {
     /*
@@ -19,10 +27,10 @@ export default defineConfig({
       cert: fs.readFileSync(path.resolve(__dirname, 'certificate.crt')),
     },
     */
-    port: process.env.FRONTEND_PORT || 3000,
-    open: true,
+    port: process.env.FRONTEND_PORT || 3000, // Puerto por defecto
+    open: true, // Abre el navegador automáticamente
     watch: {
-      usePolling: true,
+      usePolling: true, // Usa polling para observar cambios
     },
   },
 });
